173640acf43f1e322523b64faf8ff989cc3ba45b
Berkay Gürcan <46679861+berkaygurcan@users.noreply.github.com>
2022-03-29 23:57:09 +0300

    Files have been edited

--- src/App.tsx
+++ src/App.tsx
@@ -5,39 +5,27 @@ import Register from "./components/Auth/Register";
 import Login from "./components/Auth/Login";
 import BoardContent from "./components/Content/BoardContent";
 import BoardSelection from "./components/Content/BoardSelection";
-import { AppProvider } from "./context/sample-context";
+
 import ProtectedRoutes from "./components/Content/ProtectedRoutes";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [token, setToken] = useState<any>();
 
-  const getToken = () => {
-    let token = localStorage.getItem("token");
-    return token
-  }
-
-  useEffect(() => {
-    setToken(getToken())
-  },[])
- 
   return (
-    <AppProvider>
-      <Routes>
-        <Route
-          path="/login"
-          element={<Login onLogin={() => setIsLoggedIn(true)} />}
-        />
-        <Route
-          path="/register"
-          element={<Register onRegister={() => setIsLoggedIn(true)} />}
-        />
-        <Route element={<ProtectedRoutes  token = {token}/>}>
-          <Route path="/boards" element={<BoardSelection />} />
-          <Route path="/boardcontent/:id" element={<BoardContent />} />
-        </Route>
-      </Routes>
-    </AppProvider>
+    <Routes>
+      <Route
+        path="/login"
+        element={<Login onLogin={() => setIsLoggedIn(true)} />}
+      />
+      <Route
+        path="/register"
+        element={<Register onRegister={() => setIsLoggedIn(true)} />}
+      />
+      <Route element={<ProtectedRoutes />}>
+        <Route path="/boards" element={<BoardSelection />} />
+        <Route path="/boardcontent/:id" element={<BoardContent />} />
+      </Route>
+    </Routes>
   );
 }
 
--- src/components/Content/ProtectedRoutes.tsx
+++ src/components/Content/ProtectedRoutes.tsx
@@ -1,9 +1,13 @@
 import { useEffect } from "react";
 import { Navigate, Outlet } from "react-router-dom";
+import service from "../../services/odevserver/instance";
+const ProtectedRoutes = () => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    service.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  }
 
-const ProtectedRoutes = (props:any) => {
- 
-  return props.token ? <Outlet /> : <Navigate to="/login" />;
+  return token ? <Outlet /> : <Navigate to="/login" />;
 };
 
 export default ProtectedRoutes;
